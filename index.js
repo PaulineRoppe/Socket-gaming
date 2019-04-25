07f47fe0cbbf5e9f9c6ca2c92599210915e3e92c
Roppe Pauline <roppepauline@gmail.com>
2019-04-25 16:32:51 +0200

    modif for heroku

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 // io.on('connection', function(socket){
