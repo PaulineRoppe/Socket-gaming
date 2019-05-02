a9006d62ea73be17947f7587408e7ac601b90375
Roppe Pauline <roppepauline@gmail.com>
2019-05-02 12:16:29 +0200

    corrections npm

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // server.js
 // load the server resource and route GET method
 const server = require('server')
-const { get } = require('server/router')
 const { get, socket } = require('server/router')
 
 // get server port from environment or default to 3000
