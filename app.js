6ece44921d59248d6e647bf3438d9c5c5dbf3e93
Louis <cantinieauxlouis@gmail.com>
2019-05-15 09:13:28 +0200

    Nothing more

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ app.use(express.static('public'));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/MasterMind.html'));
 });
-
-
+//Mise en place des Socket et Room pour le jeux à 2
+// io.on('connection', function(socket){
+//   console.log('user connected');
+//   socket.on('disconnect', function(){
+//       console.log('user disconnected');
+//   });
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   })
+// });
 
 app.listen(port, function(){
   console.log('Running on port 8080');
